cfd3f6a1dfa6715fdd1c7932fea7872c41b0a5cd
jenny <jenny.mathew@blueconchtech.com>
2021-11-02 14:44:44 +0530

    this is second commit from Asian timezone

--- src/test/java/resources/APIResources.java
+++ src/test/java/resources/APIResources.java
@@ -3,6 +3,7 @@ package resources;
 public enum APIResources 
 {
 
+	
 	addPlaceAPI("/maps/api/place/add/json"), 
 	getPlaceAPI("/maps/api/place/get/json"), 
 	deletePlaceAPI("/maps/api/place/delete/json");
